ad01e93e68b76d2d6862534baa59586a482d2535
Andrey Fedin <mondaine.ru@yandex.ru>
2022-10-29 16:01:59 +0300

    init

--- counterApp/ViewController.swift
+++ counterApp/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var counterValue: UInt = 0
+    
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var changeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        ChangeCountValue(0)
     }
-
-
+    
+    @IBAction func buttonDidTap(_ sender: Any) {
+        counterValue += 1
+        ChangeCountValue(counterValue)
+    }
+    
+    private func ChangeCountValue (_ value: UInt) {
+        counterLabel.text = "Значение счётчика:\n" + String(counterValue)
+    }
+    
 }
 
